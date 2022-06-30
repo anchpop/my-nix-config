a4a4d3facdb12ab6b71b476a79e461c3525fdca2
Andre Popovitch <andre@popovit.ch>
2022-06-30 15:34:13 -0500

    add starship theme

--- home.nix
+++ home.nix
@@ -1,11 +1,26 @@
 { pkgs, ... }: {
     home.packages = with pkgs;
-    [ cowsay
+    [   cowsay
         fortune
         vscode
         lsd
         nerdfonts
+        starship
     ];
+    programs.starship = {
+      enable = true;
+      # Configuration written to ~/.config/starship.toml
+      settings = {
+        # add_newline = false;
+
+        # character = {
+        #   success_symbol = "[➜](bold green)";
+        #   error_symbol = "[➜](bold red)";
+        # };
+
+        # package.disabled = true;
+      };
+    };
     programs.zsh = {
       enable = true;
       shellAliases = {
